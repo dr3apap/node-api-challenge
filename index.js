cb18230e26ba7ab14453ecce934be5c09e9ddef3
Adebola  Adesina <dreapap@gmail.com>
2020-03-13 12:38:55 -0700

    Server and route setup

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const server = require("./api/server");
+
+const port = process.env.PORT || 5000;
+
+server.use(port, () => {
+  console.log(`\n***Server listening on http://localhost:${port}***\n`);
+});
